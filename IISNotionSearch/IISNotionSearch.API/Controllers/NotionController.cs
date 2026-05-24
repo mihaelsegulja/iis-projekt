dd56f7892bcafa75900a4ad3e10ba6c445d930a7
mihaelsegulja <mihael.segulja@gmail.com>
2026-05-24 10:29:46 +0200

    add notion client, generated via Kiota

---
+++ IISNotionSearch/IISNotionSearch.API/Controllers/NotionController.cs
@@ -0,0 +1,42 @@
+using IISNotionSearch.API.Abstractions.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IISNotionSearch.API.Controllers;
+
+public class NotionController : BaseController
+{
+    // public NotionController()
+    // {
+    //     
+    // }
+    //
+    // [HttpPost("search")]
+    // public Task<IActionResult> Search([FromQuery] string query)
+    // {
+    //     
+    // }
+    //
+    // [HttpGet("pages/{id}")]
+    // public Task<IActionResult> GetPage([FromRoute] string id)
+    // {
+    //     
+    // }
+    //
+    // [HttpPost("pages")]
+    // public Task<IActionResult> CreatePage([FromBody] object pageData)
+    // {
+    //     
+    // }
+    //
+    // [HttpPatch("pages/{id}")]
+    // public Task<IActionResult> UpdatePage([FromRoute] string id, [FromBody] object pageData)
+    // {
+    //     
+    // }
+    //
+    // [HttpDelete("pages/{id}")]
+    // public Task<IActionResult> DeletePage([FromRoute] string id)
+    // {
+    //     
+    // }
+}
