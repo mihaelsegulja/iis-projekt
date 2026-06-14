6dc6507470332f5d05204d1e47e93228ff06c667
mihaelsegulja <mihael.segulja@gmail.com>
2026-06-14 22:48:35 +0200

    remove unused stuff

--- IISNotionSearch/IISNotionSearch.Application/Interfaces/Common/ICurrentUserService.cs
+++
@@ -1,9 +0,0 @@
-namespace IISNotionSearch.Application.Interfaces.Common;
-
-public interface ICurrentUserService
-{
-    int? UserId { get; }
-    string? Username { get; }
-    string? Role { get; }
-    bool IsAuthenticated { get; }
-}
